36f99b0a838a0c352d39dfffbf33f2a536ec5089
nutrientroli <garcia_raul_alcazar@hotmail.com>
2019-12-13 19:30:01 +0100

    [Raul] ...

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const config = require('./config/config.js');
 const { Client } = require('discord.js');
 const bot = new Client();
 
+var desired_word = ['h','e','l','l','o'];
+var input_letters = [];
 
 bot.login(config.token);
 
@@ -10,15 +12,26 @@ bot.on('ready', () => {
 });
 
 bot.on('message', async (message) => {
-  if(message.content == '!nutrient ping'){
-    message.delete();
-    message.reply('pong');
-  }
-  if(message.content == '!nutrient hello'){
-    message.channel.send(`hello ${message.author}`);
-  }
-  if(message.content == '!nutrient clear'){
+  if(message.content == '!! clear'){
     const fetched = await message.channel.fetchMessages({limit: 100});
     message.channel.bulkDelete(fetched);
+  }else{
+    let reg = new RegExp('.. .');
+    console.log(message.content + '  ' + reg.test(message.content.toString()));
+    if(reg.test(message.content.toString())){
+      message.delete();
+      message.reply(message.author);
+    }
   }
+  /*if(message.content == '!! hello'){
+    message.channel.send(`hello ${message.author}`);
+  }*/
+
 });
+
+
+function newWord(){
+  //buscar palabra random segun dificultad
+  let word = [];
+  desired_word = word;
+}
